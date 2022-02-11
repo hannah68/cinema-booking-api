b750b101c9c6339f4adbc7fb81a03e514c0df321
hannah68 <reihaneh.naderi1989@gmail.com>
2022-02-11 15:10:24 +0000

    update code

--- src/controllers/screen.js
+++ src/controllers/screen.js
@@ -3,14 +3,17 @@ const prisma = require('../utils/prisma');
 const createScreen = async(req, res) => {
     const { number, screenings } = req.body;
     // console.log('screening', screenings);
+
+    createdScreening = {
+        createMany: {
+            data: screenings
+        }
+    }
+
     const createScreen = await prisma.screen.create({
         data: {
             number: number,
-            screenings: screenings ? {
-                createMany: {
-                    data: screenings
-                }
-            }: {}
+            screenings: screenings ? createdScreening : {}
         },
         include: {
             screenings: true
